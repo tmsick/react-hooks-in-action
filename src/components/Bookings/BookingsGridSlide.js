547177a8b14babbb18913f1854cffe7ee58214d6
Tomoya Yoshioka <tomoya.y@icloud.com>
2021-08-16 04:06:02 +0900

    Format codes with Prettier

--- src/components/Bookings/BookingsGridSlide.js
+++ src/components/Bookings/BookingsGridSlide.js
@@ -1,30 +1,26 @@
-import {useTransition, animated} from "react-spring";
+import { useTransition, animated } from "react-spring";
 
-import {shortISO} from "../../utils/date-wrangler";
+import { shortISO } from "../../utils/date-wrangler";
 
 import BookingsGrid from "./BookingsGrid";
 
-export default function BookingsGridSlide (props) {
-  const {week, bookable, booking, setBooking} = props;
+export default function BookingsGridSlide(props) {
+  const { week, bookable, booking, setBooking } = props;
 
   const transitions = useTransition(
-    {bookable, week},
-    item => `${item.bookable.id}_${shortISO(item.week.start)}`,
+    { bookable, week },
+    (item) => `${item.bookable.id}_${shortISO(item.week.start)}`,
     {
-      from: {opacity: 1, transform: 'translate3d(0, -100%, 0)'},
-      enter: {opacity: 1, transform: 'translate3d(0, 0, 0)'},
-      leave: {opacity: 0, transform: 'translate3d(0, 20%, 0)'}
+      from: { opacity: 1, transform: "translate3d(0, -100%, 0)" },
+      enter: { opacity: 1, transform: "translate3d(0, 0, 0)" },
+      leave: { opacity: 0, transform: "translate3d(0, 20%, 0)" },
     }
   );
 
   return (
     <div className="grid-wrapper">
-      {transitions.map(({item, props, key}) => (
-        <animated.div
-          className="grid"
-          style={{...props}}
-          key={key}
-        >
+      {transitions.map(({ item, props, key }) => (
+        <animated.div className="grid" style={{ ...props }} key={key}>
           <BookingsGrid
             week={item.week}
             bookable={item.bookable}
