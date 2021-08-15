002951fbb5aa1f638fbbff9eff1149f6e9e91056
Tomoya Yoshioka <tomoya.y@icloud.com>
2021-08-16 03:57:07 +0900

    Format codes with Prettier

--- src/components/Bookables/reducer.js
+++ src/components/Bookables/reducer.js
@@ -1,33 +1,32 @@
-export default function reducer (state, action) {
+export default function reducer(state, action) {
   switch (action.type) {
-
     case "SET_GROUP":
       return {
         ...state,
         group: action.payload,
-        bookableIndex: 0
+        bookableIndex: 0,
       };
 
     case "SET_BOOKABLE":
       return {
         ...state,
-        bookableIndex: action.payload
+        bookableIndex: action.payload,
       };
 
     case "TOGGLE_HAS_DETAILS":
       return {
         ...state,
-        hasDetails: !state.hasDetails
+        hasDetails: !state.hasDetails,
       };
 
     case "NEXT_BOOKABLE":
       const count = state.bookables.filter(
-        b => b.group === state.group
+        (b) => b.group === state.group
       ).length;
 
       return {
         ...state,
-        bookableIndex: (state.bookableIndex + 1) % count
+        bookableIndex: (state.bookableIndex + 1) % count,
       };
 
     default:
