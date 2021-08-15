b65bafe2991c215a5c0dfacaf72f771161a17a8e
Tomoya Yoshioka <tomoya.y@icloud.com>
2021-08-16 03:59:51 +0900

    Format codes with Prettier

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,48 +1,47 @@
-import {useState, useEffect, useRef} from 'react';
-import {FaArrowRight} from "react-icons/fa";
+import { useState, useEffect, useRef } from "react";
+import { FaArrowRight } from "react-icons/fa";
 import Spinner from "../UI/Spinner";
 import getData from "../../utils/api";
 
-export default function BookablesList ({bookable, setBookable}) {
+export default function BookablesList({ bookable, setBookable }) {
   const [bookables, setBookables] = useState([]);
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
   const group = bookable?.group;
 
-  const bookablesInGroup = bookables.filter(b => b.group === group);
-  const groups = [...new Set(bookables.map(b => b.group))];
+  const bookablesInGroup = bookables.filter((b) => b.group === group);
+  const groups = [...new Set(bookables.map((b) => b.group))];
 
   const nextButtonRef = useRef();
 
   useEffect(() => {
     getData("http://localhost:3001/bookables")
-
-      .then(bookables => {
+      .then((bookables) => {
         setBookable(bookables[0]);
         setBookables(bookables);
         setIsLoading(false);
       })
 
-      .catch(error => {
+      .catch((error) => {
         setError(error);
         setIsLoading(false);
       });
   }, [setBookable]);
 
-  function changeGroup (e) {
+  function changeGroup(e) {
     const bookablesInSelectedGroup = bookables.filter(
-      b => b.group === e.target.value
+      (b) => b.group === e.target.value
     );
     setBookable(bookablesInSelectedGroup[0]);
   }
 
-  function changeBookable (selectedBookable) {
+  function changeBookable(selectedBookable) {
     setBookable(selectedBookable);
     nextButtonRef.current.focus();
   }
 
-  function nextBookable () {
+  function nextBookable() {
     const i = bookablesInGroup.indexOf(bookable);
     const nextIndex = (i + 1) % bookablesInGroup.length;
     const nextBookable = bookablesInGroup[nextIndex];
@@ -50,29 +49,31 @@ export default function BookablesList ({bookable, setBookable}) {
   }
 
   if (error) {
-    return <p>{error.message}</p>
+    return <p>{error.message}</p>;
   }
 
   if (isLoading) {
-    return <p><Spinner/> Loading bookables...</p>
+    return (
+      <p>
+        <Spinner /> Loading bookables...
+      </p>
+    );
   }
 
   return (
     <div>
       <select value={group} onChange={changeGroup}>
-        {groups.map(g => <option value={g} key={g}>{g}</option>)}
+        {groups.map((g) => (
+          <option value={g} key={g}>
+            {g}
+          </option>
+        ))}
       </select>
 
       <ul className="bookables items-list-nav">
-        {bookablesInGroup.map(b => (
-          <li
-            key={b.id}
-            className={b.id === bookable.id ? "selected" : null}
-          >
-            <button
-              className="btn"
-              onClick={() => changeBookable(b)}
-            >
+        {bookablesInGroup.map((b) => (
+          <li key={b.id} className={b.id === bookable.id ? "selected" : null}>
+            <button className="btn" onClick={() => changeBookable(b)}>
               {b.title}
             </button>
           </li>
@@ -85,7 +86,7 @@ export default function BookablesList ({bookable, setBookable}) {
           ref={nextButtonRef}
           autoFocus
         >
-          <FaArrowRight/>
+          <FaArrowRight />
           <span>Next</span>
         </button>
       </p>
