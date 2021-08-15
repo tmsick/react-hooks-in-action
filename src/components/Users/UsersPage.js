b30584188b81af911b341622dc4f0be97637f0f0
Tomoya Yoshioka <tomoya.y@icloud.com>
2021-08-16 04:01:15 +0900

    Format codes with Prettier

--- src/components/Users/UsersPage.js
+++ src/components/Users/UsersPage.js
@@ -1,10 +1,10 @@
-import {useState, useContext} from "react";
+import { useState, useContext } from "react";
 import UsersList from "./UsersList";
 import UserDetails from "./UserDetails";
 
 import UserContext from "./UserContext"; // import the shared context
 
-export default function UsersPage () {
+export default function UsersPage() {
   const [user, setUser] = useState(null);
 
   // get the user from context
@@ -17,8 +17,8 @@ export default function UsersPage () {
   // pass currentUser to children
   return (
     <main className="users-page">
-      <UsersList user={currentUser} setUser={setUser}/>
-      <UserDetails user={currentUser}/>
+      <UsersList user={currentUser} setUser={setUser} />
+      <UserDetails user={currentUser} />
     </main>
   );
 }
