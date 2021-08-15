5d6d7a890fa78200f7081c1c3da3f8704c45aa3d
Tomoya Yoshioka <tomoya.y@icloud.com>
2021-08-16 03:55:50 +0900

    Format codes with Prettier

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,48 +1,39 @@
-import {useState} from 'react';
-import {bookables} from "../../static.json";
-import {FaArrowRight} from "react-icons/fa";
+import { useState } from "react";
+import { bookables } from "../../static.json";
+import { FaArrowRight } from "react-icons/fa";
 
-export default function BookablesList () {
+export default function BookablesList() {
   const [group, setGroup] = useState("Kit");
-  const bookablesInGroup = bookables.filter(b => b.group === group);
+  const bookablesInGroup = bookables.filter((b) => b.group === group);
   const [bookableIndex, setBookableIndex] = useState(0);
-  const groups = [...new Set(bookables.map(b => b.group))];
+  const groups = [...new Set(bookables.map((b) => b.group))];
 
-  function nextBookable () {
-    setBookableIndex(i => (i + 1) % bookablesInGroup.length);
+  function nextBookable() {
+    setBookableIndex((i) => (i + 1) % bookablesInGroup.length);
   }
 
   return (
     <div>
-      <select
-        value={group}
-        onChange={(e) => setGroup(e.target.value)}
-      >
-        {groups.map(g => <option value={g} key={g}>{g}</option>)}
+      <select value={group} onChange={(e) => setGroup(e.target.value)}>
+        {groups.map((g) => (
+          <option value={g} key={g}>
+            {g}
+          </option>
+        ))}
       </select>
 
       <ul className="bookables items-list-nav">
         {bookablesInGroup.map((b, i) => (
-          <li
-            key={b.id}
-            className={i === bookableIndex ? "selected" : null}
-          >
-            <button
-              className="btn"
-              onClick={() => setBookableIndex(i)}
-            >
+          <li key={b.id} className={i === bookableIndex ? "selected" : null}>
+            <button className="btn" onClick={() => setBookableIndex(i)}>
               {b.title}
             </button>
           </li>
         ))}
       </ul>
       <p>
-        <button
-          className="btn"
-          onClick={nextBookable}
-          autoFocus
-        >
-          <FaArrowRight/>
+        <button className="btn" onClick={nextBookable} autoFocus>
+          <FaArrowRight />
           <span>Next</span>
         </button>
       </p>
