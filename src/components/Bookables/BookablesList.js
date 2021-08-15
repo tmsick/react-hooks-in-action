eab910d4710ba5124aef3f5f7e9cd72db67e5e2f
Tomoya Yoshioka <tomoya.y@icloud.com>
2021-08-16 03:55:35 +0900

    Format codes with Prettier

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,40 +1,30 @@
-import {useState} from 'react';
-import {bookables} from "../../static.json";
-import {FaArrowRight} from "react-icons/fa";
+import { useState } from "react";
+import { bookables } from "../../static.json";
+import { FaArrowRight } from "react-icons/fa";
 
-export default function BookablesList () {
+export default function BookablesList() {
   const group = "Rooms";
-  const bookablesInGroup = bookables.filter(b => b.group === group);
+  const bookablesInGroup = bookables.filter((b) => b.group === group);
   const [bookableIndex, setBookableIndex] = useState(1);
 
-  function nextBookable () {
-    setBookableIndex(i => (i + 1) % bookablesInGroup.length);
+  function nextBookable() {
+    setBookableIndex((i) => (i + 1) % bookablesInGroup.length);
   }
 
   return (
     <div>
       <ul className="bookables items-list-nav">
         {bookablesInGroup.map((b, i) => (
-          <li
-            key={b.id}
-            className={i === bookableIndex ? "selected" : null}
-          >
-            <button
-              className="btn"
-              onClick={() => setBookableIndex(i)}
-            >
+          <li key={b.id} className={i === bookableIndex ? "selected" : null}>
+            <button className="btn" onClick={() => setBookableIndex(i)}>
               {b.title}
             </button>
           </li>
         ))}
       </ul>
       <p>
-        <button
-          className="btn"
-          onClick={nextBookable}
-          autoFocus
-        >
-          <FaArrowRight/>
+        <button className="btn" onClick={nextBookable} autoFocus>
+          <FaArrowRight />
           <span>Next</span>
         </button>
       </p>
--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,20 +1,14 @@
-import {useState} from 'react';
-import {users} from "../../static.json";
+import { useState } from "react";
+import { users } from "../../static.json";
 
-export default function UsersList () {
+export default function UsersList() {
   const [userIndex, setUserIndex] = useState(0);
 
   return (
     <ul className="users items-list-nav">
       {users.map((u, i) => (
-        <li
-          key={u.id}
-          className={i === userIndex ? "selected" : null}
-        >
-          <button
-            className="btn"
-            onClick={() => setUserIndex(i)}
-          >
+        <li key={u.id} className={i === userIndex ? "selected" : null}>
+          <button className="btn" onClick={() => setUserIndex(i)}>
             {u.name}
           </button>
         </li>
