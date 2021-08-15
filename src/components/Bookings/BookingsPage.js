e41a7133ff0f5b84a0667ea669c0160c36f720df
Tomoya Yoshioka <tomoya.y@icloud.com>
2021-08-16 04:03:29 +0900

    Format codes with Prettier

--- src/components/Bookings/BookingsPage.js
+++ src/components/Bookings/BookingsPage.js
@@ -1,33 +1,33 @@
 import useFetch from "../../utils/useFetch";
-import {shortISO} from "../../utils/date-wrangler";
-import {useBookingsParams} from "./bookingsHooks";
+import { shortISO } from "../../utils/date-wrangler";
+import { useBookingsParams } from "./bookingsHooks";
 
 import BookablesList from "../Bookables/BookablesList";
 import Bookings from "./Bookings";
 import PageSpinner from "../UI/PageSpinner";
 
-export default function BookingsPage () {
-  const {data: bookables = [], status, error} = useFetch(
-    "http://localhost:3001/bookables"
-  );
+export default function BookingsPage() {
+  const {
+    data: bookables = [],
+    status,
+    error,
+  } = useFetch("http://localhost:3001/bookables");
 
-  const {date, bookableId} = useBookingsParams();
+  const { date, bookableId } = useBookingsParams();
 
-  const bookable = bookables.find(
-    b => b.id === bookableId
-  ) || bookables[0];
+  const bookable = bookables.find((b) => b.id === bookableId) || bookables[0];
 
-  function getUrl (id) {
+  function getUrl(id) {
     const root = `/bookings?bookableId=${id}`;
     return date ? `${root}&date=${shortISO(date)}` : root;
   }
 
   if (status === "error") {
-    return <p>{error.message}</p>
+    return <p>{error.message}</p>;
   }
 
   if (status === "loading") {
-    return <PageSpinner/>
+    return <PageSpinner />;
   }
 
   return (
@@ -37,9 +37,7 @@ export default function BookingsPage () {
         bookables={bookables}
         getUrl={getUrl}
       />
-      <Bookings
-        bookable={bookable}
-      />
+      <Bookings bookable={bookable} />
     </main>
   );
 }
--- src/components/Bookings/bookingsHooks.js
+++ src/components/Bookings/bookingsHooks.js
@@ -1,59 +1,61 @@
-import {useMemo} from "react";
-import {useSearchParams} from "react-router-dom";
-import {shortISO, isDate} from "../../utils/date-wrangler";
+import { useMemo } from "react";
+import { useSearchParams } from "react-router-dom";
+import { shortISO, isDate } from "../../utils/date-wrangler";
 import useFetch from "../../utils/useFetch";
-import {getGrid, transformBookings} from "./grid-builder";
+import { getGrid, transformBookings } from "./grid-builder";
 
-export function useBookings (bookableId, startDate, endDate) {
+export function useBookings(bookableId, startDate, endDate) {
   const start = shortISO(startDate);
   const end = shortISO(endDate);
 
   const urlRoot = "http://localhost:3001/bookings";
 
-  const queryString = `bookableId=${bookableId}` +
-    `&date_gte=${start}&date_lte=${end}`;
+  const queryString =
+    `bookableId=${bookableId}` + `&date_gte=${start}&date_lte=${end}`;
 
   const query = useFetch(`${urlRoot}?${queryString}`);
 
   return {
     bookings: query.data ? transformBookings(query.data) : {},
-    ...query
+    ...query,
   };
 }
 
-export function useGrid (bookable, startDate) {
+export function useGrid(bookable, startDate) {
   return useMemo(
-    () => bookable ? getGrid(bookable, startDate) : {},
+    () => (bookable ? getGrid(bookable, startDate) : {}),
     [bookable, startDate]
   );
 }
 
-export function useBookingsParams () {
+export function useBookingsParams() {
   const [searchParams, setSearchParams] = useSearchParams();
   const searchDate = searchParams.get("date");
   const bookableId = searchParams.get("bookableId");
 
-  const date = isDate(searchDate)
-    ? new Date(searchDate)
-    : new Date();
+  const date = isDate(searchDate) ? new Date(searchDate) : new Date();
 
   const idInt = parseInt(bookableId, 10);
   const hasId = !isNaN(idInt);
 
-  function setBookingsDate (date) {
+  function setBookingsDate(date) {
     const params = {};
 
-    if (hasId) {params.bookableId = bookableId}
-    if (isDate(date)) {params.date = date}
+    if (hasId) {
+      params.bookableId = bookableId;
+    }
+    if (isDate(date)) {
+      params.date = date;
+    }
 
     if (params.date || params.bookableId !== undefined) {
-      setSearchParams(params, {replace: true});
+      setSearchParams(params, { replace: true });
     }
   }
 
   return {
     date,
     bookableId: hasId ? idInt : undefined,
-    setBookingsDate
+    setBookingsDate,
   };
 }
